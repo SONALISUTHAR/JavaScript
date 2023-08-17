00ca039f94f2eb98a79e4d37c05e939413e37089
SONALI SUTHAR <127579102+SONALISUTHAR@users.noreply.github.com>
2023-08-17 15:53:09 +0530

    Add files via upload

---
+++ Hoisting and tdz/index.js
@@ -0,0 +1,74 @@
+/////////Hoisting /////////////
+
+
+console.log(addDecl(6,3));
+
+function addDecl(a,b){
+    return a+ b;
+}
+
+
+////****** HOSITING EXAMPLE ******//
+
+//console.log(a)
+greet()
+function greet(){
+    console.log('hey')
+    
+}
+ var a = 5;
+
+console.log(a)
+
+
+
+///// example/////
+
+
+function fun (){
+    let name = 'aayushi suthar';
+    console.log(name)
+  
+}
+fun()
+
+////////////// EXAMPLE ///////////////////
+
+
+
+var a = 4;
+
+function greet() {
+    b = 'VALUE IS';
+    console.log(b); 
+    var b;
+}
+console.log(a);
+
+
+
+ /////////********** */ Temporal Dead Zone************ ///////
+
+ console.log(varNumber)        //undefined
+ //console.log(letNumber)
+
+ var varNumber = 1;
+
+ //let letNumber = 1;
+
+
+ ///////***** SECOND EXAMPLE ******//////////
+ 
+ 
+ var babyAge = 10;
+ var isBirthday = true;  /// var first value has been used and re-declaration var inside the 'if' 
+
+if(isBirthday) {
+	var babyAge = 11; 
+}
+console.log(babyAge);
+
+
+
+/////////  example /////////
+
