bf31d2d59603d59b8431b9592bb76a28d35c2e62
SONALI SUTHAR <127579102+SONALISUTHAR@users.noreply.github.com>
2023-08-17 13:31:49 +0530

    Create Scoping.js

---
+++ Scoping.js
@@ -0,0 +1,29 @@
+function calAge(birthYear){
+    const age = 2023 - birthYear;
+
+    function updateAge(){    
+    const output = `${firstName}, you are ${age} , born in ${birthYear}`
+    console.log(output);
+
+ if (birthYear >= 1987 && birthYear <= 2026){
+    var millenial = true;
+    const firstName = 'Suthar';
+    const str = `oh, and you're a millenial, ${firstName}`;
+    console.log(str);
+
+    function add(a, b){
+        return a + b;
+    }
+    
+ }
+
+console.log(millenial);
+console.log(output);
+console.log(add(2,3));
+}
+updateAge();
+return age;
+}
+const firstName = 'sonali';
+calAge(1997);
+
